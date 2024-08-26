ae70be9df725fa95622263816afa10bf848b037d
anndrelipe <ppeixoto.andre@gmail.com>
2024-08-26 13:55:24 -0300

    Adiciona cors para uso externo da api e edita o readme

--- src/config/app.js
+++ src/config/app.js
@@ -1,8 +1,16 @@
 import express from "express";
 import productsRouter from "../routes/productsRouter.js";
+import cors from "cors";
+
 const app = express();
 
-app.use(express.json(), productsRouter);
+const corsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}
+
+app.use(express.json(), cors(corsOptions), productsRouter);
 
 app.get('/', (req, res) => {
     res.send('Olá world!');
