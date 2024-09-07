2f0f093ecc04075a0b19211c54ced55cc5420700
Raylan Cordeiro de Souza <raylan.cordeiro@DIGITALCOLLEGE.LOCAL>
2024-09-06 21:44:15 -0300

    implements swagger docs

--- src/config/app.js
+++ src/config/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import productsRouter from "../routes/productsRouter.js";
 import cors from "cors";
-
 const app = express();
 
 const corsOptions = {
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,8 +1,15 @@
 import  ProductsController  from "../controllers/ProductsController.js";
 import { Router } from "express";
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 const productsRouter = Router();
 
+//TODO: Deixar o path dinamico
+const swaggerDocument = YAML.load('/app/src/routes/swagger.yaml');
+
+productsRouter.use('/api-docs', swaggerUi.serve);
+productsRouter.get('/api-docs', swaggerUi.setup(swaggerDocument));
 productsRouter.get('/products', ProductsController.getAllProducts);
 productsRouter.post('/products', ProductsController.createNewProduct);
 productsRouter.delete('/products/:id', ProductsController.deleteProducts);
